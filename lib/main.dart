0884cbc96365b7691917b464e3ed3f404e89c6d7
Satyam Gupta <satyamx811@gmail.com>
2022-02-22 19:22:18 +0530

    Day 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,27 @@
-import 'package:basic_flutter/home_page.dart';
+import 'package:basic_flutter/pages/home_page.dart';
+import 'package:basic_flutter/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main(){
-
-  runApp(Myapp());
+  runApp(MyApp());
 }
 
-class Myapp extends StatelessWidget {
-  const Myapp({ Key? key }) : super(key: key);
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      home: HomePage(),
-      
+      themeMode: ThemeMode.light, 
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.light
+        ), 
+        initialRoute: "/home",
+        routes: {
+          "/home" : (context)=> HomePage(),
+          "/login" :(context)=> LoginPage(),
+        },
     );
   }
 }
